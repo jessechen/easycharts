53d6a3805ae46775aaf87a6e5f5798b38bd2a666
Jesse Chen <jesse.z.chen@gmail.com>
2015-06-17 17:35:03 -0400

    Added new data source for json chart.

--- json-chart.js
+++ json-chart.js
@@ -2,16 +2,13 @@ $(function () {
     $.get('data.json', function(file) {
         $('#chart').highcharts({
             chart: {zoomType: 'x'},
-            title: {text: 'WoW Token Prices'},
+            title: {text: 'Microphone signal amplitude'},
             series: [{
-                pointInterval: 60 * 60 * 1000, // 1 hour
-                pointStart: 1428426758,
                 data: file.data
             }],
             credits: {enabled: false},
             legend: {enabled: false},
-            yAxis: {title: {text: 'Gold'}},
-            xAxis: {type: 'datetime'}
+            yAxis: {title: {enabled: false}}
         });
     });
 });
